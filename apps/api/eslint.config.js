cf21ae974ea261f713af2bf754e16440505f2b4f
obeim <obei.m2017@gmail.com>
2025-08-17 12:36:30 +0300

    fix(web): add nextjs config to build & remove uneeded files

--- apps/api/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { config as base } from "@repo/eslint-config/base";
-
-/** @type {import("eslint").Linter.Config} */
-export default [...base];
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,9 +8,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*",
-        port: "",
-        search: "",
+        hostname: "**.mzstatic.com",
+        pathname: "/**",
       },
     ],
   },
